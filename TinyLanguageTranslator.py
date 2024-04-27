8ba46f8bd2e576216bebf89f0048f7c490384810
kaser <qaz442200156@gmail.com>
2024-04-28 05:00:50 +0900

    keep it simple to use

--- TinyLanguageTranslator.py
+++ TinyLanguageTranslator.py
@@ -18,33 +18,26 @@ import time
 translator = Translator()
 tool = language_tool_python.LanguageTool('en-US')
 
-setting_keep_log = False
 setting_auto_save_log = True
-skip_clean_console = False
 # use `global`` to access global variable
 
+# source language
 src_language = 'en'
+# language to translate
 dest_language = 'zh-Tw'
 
 def check_help(command):
-    global skip_clean_console
     if "-h" in command:
         print("-s to check on used settings")
-        print("-log to Keep old log")
         print("-AutoSave to Auto Save Log into text file")
         print("Sample:-log -AutoSave")
         command = ""
-        skip_clean_console = True
         return True
     return False
 
 def check_setting(command):
-    global setting_keep_log
     global setting_auto_save_log
     any_change = False
-    if "-log" in command.lower():
-        setting_keep_log = setting_keep_log is not True
-        any_change = True
     if "-autosave" in command.lower():
         setting_auto_save_log = setting_auto_save_log is not True
         any_change = True
@@ -52,41 +45,33 @@ def check_setting(command):
     return any_change
 
 def show_setting_state(command,force_show=False):
-    global skip_clean_console
-    global setting_keep_log
     global setting_auto_save_log
     if "-s" in command.lower() or force_show:
-        if setting_keep_log:
-            print("Keep Log Is On")
-        else:
-            print("Keep Log Is Off")
         if setting_auto_save_log:
             print("Auto Save Log Is On")
         else:
             print("Auto Save Log Is Off")
-        skip_clean_console = True
         return True
     return False
 
 # Clean console output
-def clean_console(force = False):
-    global skip_clean_console
-    if skip_clean_console:
-        skip_clean_console = False
-        return
-    if not setting_keep_log or force:
-        os.system('cls' if os.name == 'nt' else 'clear')
+def clean_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def translate_world(world):
+    global src_language
+    global dest_language
     translated = translator.translate(world, src=src_language, dest=dest_language)
-    result = "{src_language}:"+world+" -> {dest_language}:"+translated.text
+    result = f"{src_language}:"+world+f" -> {dest_language}:"+translated.text
     print(result)
     return result
 
 def correct_grammar_and_translate(sentence):    
+    global src_language
+    global dest_language
     corrected_text = language_tool_python.utils.correct(sentence,matches)
     translated = translator.translate(sentence, src=src_language, dest=dest_language)
-    result = "{src_language}:"+sentence+" #[Fix]:"+corrected_text+" -> {dest_language}:"+translated.text
+    result = f"{src_language}:"+sentence+" #[Fix]:"+corrected_text+f" -> {dest_language}:"+translated.text
     print(result)
     return result
 
@@ -103,14 +88,12 @@ def command_reader(command):
     elif command.lower() == 'e':
         return False
     elif command.lower() == 'cls':
-        clean_console(True)
+        clean_console()
         return True
     elif command.lower() == 'w':
-        clean_console()
         command = input("Enter a world (or 'e' to quit): ")
         result = translate_world(command)
     elif command.lower() == 's':
-        clean_console()
         command = input("Enter a sentence (or 'e' to quit): ")
         matches = tool.check(command)
         if len(matches) == 0:
@@ -123,9 +106,7 @@ def command_reader(command):
     return True
 
 def main():
-    while True:    
-        clean_console()
-        
+    while True:
         command = input("Enter S(sentence) or W(world) to set translate mode (or 'e' to quit '-h' for help): ")
         if check_help(command):
             continue
